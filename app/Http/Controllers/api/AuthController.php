779ce35629ecdf3db63cf7ff98211f365fae6df4
Momen9Sarsour <momensarsour5@gmail.com>
2023-09-20 11:14:46 +0300

    Edit of api register

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
+            return $this->apiResponse(['errors' => $validator->errors()], 'invaled' , 422);
         }
 
         $data = $request->except('image');
